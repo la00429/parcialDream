fad179719f73e3e7cd5c146fe1f3240d0077a12a
Alejandro Forero <alejandroforero012@gmail.com>
2025-04-07 19:49:12 -0500

    Implementado el factory method

--- src/main/java/co/edu/uptc/factorymethod/DreamRepositoryFactory.java
+++ src/main/java/co/edu/uptc/factorymethod/DreamRepositoryFactory.java
@@ -1,8 +1,11 @@
 package co.edu.uptc.factorymethod;
 
 public abstract class DreamRepositoryFactory {
-
-    public void create(String typeRepository){
-
+    public static DreamRepository createRepository(String type) {
+        return switch (type.toLowerCase()) {
+            case "historial", "historical" -> new DreamHistory();
+            case "temporal", "temporary" -> new DreamTemporal();
+            default -> throw new IllegalArgumentException("Tipo de repositorio no válido: " + type);
+        };
     }
 }
